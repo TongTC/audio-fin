59318c8e9ee46000a1dce3760b8c1f7de6c9e060
nata thip <nataphong.tt@gmail.com>
2025-05-15 14:05:44 +0700

    add not found page

--- src/app/app.route.ts
+++ src/app/app.route.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AvailableStocksComponent } from './stocks/available-stocks/available-stocks.component';
 import { ProductsdetailComponent } from './productsdetail/productsdetail.component';
+import { NotfoundComponent } from './notfound/notfound.component';
 
 export const routes: Routes = [
   {path:'home',component:HomeComponent},
   {path:'allstock',component:AvailableStocksComponent},
   {path:'details',component:ProductsdetailComponent},
-  {path:'',component:HomeComponent}
+  {path:'',component:HomeComponent},
+  {path:'**',component:NotfoundComponent}
 ];
 //test
