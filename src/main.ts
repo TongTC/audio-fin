e55015aa6324ab4a23e6f8d7fd2bd8dcebae832a
nata thip <nataphong.tt@gmail.com>
2025-05-15 15:10:29 +0700

    config prettier

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,15 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withRouterConfig } from '@angular/router';
 import { routes } from './app/app.route';
 
-
-bootstrapApplication(AppComponent
-  ,{ providers:[ provideRouter(routes),provideHttpClient()] }
-).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(
+      routes,
+      withRouterConfig({ paramsInheritanceStrategy: 'always' })
+    ),
+    provideHttpClient(),
+  ],
+}).catch((err) => console.error(err));
