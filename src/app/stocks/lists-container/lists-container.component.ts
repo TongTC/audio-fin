d8a02568b1657b6f665755b6ab32471c1753d6f7
nata thip <nataphong.tt@gmail.com>
2025-05-15 17:18:51 +0700

    add children router

--- src/app/stocks/lists-container/lists-container.component.ts
+++ src/app/stocks/lists-container/lists-container.component.ts
@@ -5,7 +5,7 @@ import { Component, input } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './lists-container.component.html',
-  styleUrl: './lists-container.component.css'
+  styleUrl: './lists-container.component.css',
 })
 export class ListsContainerComponent {
   title = input.required<string>();
