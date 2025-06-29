3837abdb056a3cdaf8a3bad9dad7246c9ede01b4
Nata Thip <nataphong.tt@gmail.com>
2025-06-29 22:46:40 +0700

    corect something

--- src/app/grid-layout/grid-layout.component.ts
+++ src/app/grid-layout/grid-layout.component.ts
@@ -19,7 +19,7 @@ export interface Tile {
 })
 export class GridLayoutComponent {
   tiles: Tile[] = [
-    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'When you found the stock needed', cols: 3, rows: 1, color: 'lightblue'},
     {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
     {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
     {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
